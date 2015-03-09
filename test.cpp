a50e3f712d3b0e3539492ee53a507b25c7a11311
Pitolin Kevin <kpitolin@if501-219-10.insa-lyon.fr>
2015-03-09 12:02:27 +0100

    const begun

--- test.cpp
+++ test.cpp
@@ -6,27 +6,35 @@ using namespace std;
 using namespace boost;
 
 // Show result
-void lexical_result (string s, bool b)
+
+
+const string ID_REGEX = "\\w+";
+const string VAL_REGEX =;
+
+bool lexical_result (string s, bool b)
 {
 	if (b){
 		cout << "Expression " +s+" correcte" << endl;
 	}else{
 		cout << "Expression "+s + " incorrecte" << endl ;
 	}
+	
+	return b;
 }
 
 bool test_declaration(const string &s)
 {
 	const regex declaration("(var|const) \\w+");
-	lexical_result(s,regex_match(s,declaration));
 
-	return regex_match(s, declaration);
+	return lexical_result(s,regex_match(s,declaration));
+;
 }
 
 bool test_affectation(const string &s)
 {
 	const regex affectation("(var|const) \\w+ = [-+]?[0-9]*\.?[0-9]+$");
-	return regex_match(s,affectation);
+	return lexical_result(s,regex_match(s,affectation));
+
 }
 
  // verify the validity of multiplication operator 
@@ -35,9 +43,30 @@ bool test_opM(const string &s)
 {
 	const regex opM("(\\*|/)");
 	
-	 lexical_result(s,regex_match(s,opM));
 
-	return regex_match(s,opM);
+	return lexical_result(s,regex_match(s,opM));
+
+	
+}
+ // verify the validity of addition operator 
+
+bool test_opA(const string &s)
+{
+	const regex opA("(+|-)");
+	
+
+	return lexical_result(s,regex_match(s,opA));
+
+	
+}
+
+bool test_instruction(const string &s)
+{
+	const regex instruction("(lire|ecrire) \\w+");
+	
+
+	return lexical_result(s,regex_match(s,instruction));
+
 }
 
 
@@ -48,7 +77,7 @@ bool test_opM(const string &s)
 
 int main()
 {
-	const regex opA("(?: +|-)");
+	const regex opA("(+|-)");
 	const regex opM("(\\*|/)");
 	const regex expression("(?: expression|\\v)(?: opM|opA)(?: expression|\\v)");
 	const regex priority("(\expression)");
@@ -59,14 +88,18 @@ int main()
 	
 	cout << "test declaration" << endl;
 	
-	string declare = "var l";
-	cout << test_declaration(declare) << endl;
-	
+	string affect = "var l";
+	 test_declaration(affect) ;
 	
 	cout << "test affectation" << endl;
 	
-	string affect = "var hehe = c'est faux";
-	cout << test_affectation(affect) << endl;
+	 affect = "var hehe = c'est faux";
+	test_affectation(affect);
+	
+		cout << "test instruction" << endl;
+	
+	 affect = "lire hehe";
+	test_instruction(affect);
 	
 	return 0;
 }
