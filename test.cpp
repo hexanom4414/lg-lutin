76b9587f6a97eabd98664989a173a07391dd8266
Nguyen Tuan Hoang <tnguyen1@if501-219-09.insa-lyon.fr>
2015-03-09 09:47:44 +0100

    ok pour affectation

--- test.cpp
+++ test.cpp
@@ -8,28 +8,33 @@ using namespace boost;
 bool test_declaration(const string &s)
 {
 	const regex declaration("(var|const) \\w+");
-	return regex_match(s, declaration);
+	return regex_match(s,declaration);
 }
 
-bool test_opA(const string &s)
+bool test_affectation(const string &s)
 {
-	const regex opA("(?: +|-)");
-	return regex_match
+	const regex affectation("(var|const) \\w+ = [-+]?[0-9]*\.?[0-9]+$");
+	return regex_match(s,affectation);
 }
 
 int main()
 {
-	const regex affectation("\\v[:=]\\val|(?: var|const) \\v[=]\\val");
 	const regex opA("(?: +|-)");
 	const regex opM("(?: *|/)");
 	const regex expression("(?: expression|\\v)(?: opM|opA)(?: expression|\\v)");
 	const regex priority("(\expression)");
 	
 
-	cout << "test 1" << endl;
+	cout << "test declaration" << endl;
 	
-	string declare = "var lol";
+	string declare = "var l";
 	cout << test_declaration(declare) << endl;
+	
+	
+	cout << "test affectation" << endl;
+	
+	string affect = "var hehe = c'est faux";
+	cout << test_affectation(affect) << endl;
 	
 	return 0;
 }
