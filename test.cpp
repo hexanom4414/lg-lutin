a8644fc2e81df6b2a2c695c8723318294c9890ac
Pitolin Kevin <kpitolin@if501-219-10.insa-lyon.fr>
2015-03-09 11:22:03 +0100

    opM & opA added

--- test.cpp
+++ test.cpp
@@ -5,31 +5,61 @@
 using namespace std;
 using namespace boost;
 
+// Show result
+void lexical_result (string s, bool b)
+{
+	if (b){
+		cout << "Expression " +s+" correcte" << endl;
+	}else{
+		cout << "Expression "+s + " incorrecte" << endl ;
+	}
+}
+
 bool test_declaration(const string &s)
 {
 	const regex declaration("(var|const) \\w+");
+	lexical_result(s,regex_match(s,declaration));
+
 	return regex_match(s, declaration);
 }
 
 bool test_opA(const string &s)
 {
-	const regex opA("(?: +|-)");
-	return regex_match
+	const regex opA("(+|-)");
+	lexical_result(s,regex_match(s,opA));
+
+	return regex_match(s,opA);
 }
+
+ // verify the validity of multiplication operator 
+
+bool test_opM(const string &s)
+{
+	const regex opM("(\\*|/)");
+	
+	 lexical_result(s,regex_match(s,opM));
+
+	return regex_match(s,opM);
+}
+
+
+
+
+
+
 
 int main()
 {
 	const regex affectation("\\v[:=]\\val|(?: var|const) \\v[=]\\val");
 	const regex opA("(?: +|-)");
-	const regex opM("(?: *|/)");
+	const regex opM("(\\*|/)");
 	const regex expression("(?: expression|\\v)(?: opM|opA)(?: expression|\\v)");
 	const regex priority("(\expression)");
 	
 
-	cout << "test 1" << endl;
+
+	test_opM("*");
 	
-	string declare = "var lol";
-	cout << test_declaration(declare) << endl;
 	
 	return 0;
 }
