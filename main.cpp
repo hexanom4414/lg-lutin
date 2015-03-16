db47f64f6f8aa5206eb48a4f4ce0f395cd2e2fa0
rlatapy <remi.latapy@insa-lyon.fr>
2015-03-16 10:46:38 +0100

    Add tests

--- main.cpp
+++ main.cpp
@@ -8,5 +8,14 @@ int main(int argc, const char* argv[])
 {
     cout << "Hello" << endl;
 
+    // argc test
+    cout << "Nombre d'arguments : " << argc << endl;
+
+    // argv treatment
+    for (int x = 0; x < argc; ++x)
+    {
+        cout << "Argument " << x << " : " << argv[x] << endl;
+    }
+
     return 0;
 }
