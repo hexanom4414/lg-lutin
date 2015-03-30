23115b592611c446ec718f20dc877312b8e066fd
RemiLatapy <remi.latapy@insa-lyon.fr>
2015-03-30 15:17:23 +0200

    fix to compil

--- src/Symboles/Programme.cpp
+++ src/Symboles/Programme.cpp
@@ -16,33 +16,17 @@ Programme::Programme (const Programme & p) : SymboleComplexe (p)
 
 {
 	m_listInstruction = p.m_listInstruction;
-	m_listDeclaration = p.m_listDeclaration; 
+	m_listDeclaration = p.m_listDeclaration;
 }
 
 void Programme::execute()
 
 {
 	// Here we fill in the map
-	
-	
-	    for (list<Declaration *>::reverse_iterator it=m_listDeclaration.m_listDeclaration.rbegin(); it != m_listDeclaration.m_listDeclaration.rend(); ++it)
-	    {
-			(*it)->execute();
-		}
+	m_listDeclaration->execute();
 
-	
-	
 	//Here we execute the instructions
-	
-    for (list<Instruction *>::reverse_iterator it=m_listInstruction.m_listInstruction.rbegin(); it != m_listInstruction.m_listInstruction.rend(); ++it)
-    
-    {
-		
-		(*it)->execute;
-	}
-
-	
-	
+	m_listInstruction->execute();
 }
 
 void Programme::transformation()
